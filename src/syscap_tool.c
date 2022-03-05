0053e35560e7aead623ed4da5e30b511e0b6e385
lnlan <lanleinan@163.com>
2022-03-05 12:00:52 +0000

    add test
    Signed-off-by: lnlan <lanleinan3@huawei.com>

--- src/syscap_tool.c
+++ src/syscap_tool.c
@@ -1,47 +1,32 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd. All rights reserved.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of
- *    conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list
- *    of conditions and the following disclaimer in the documentation and/or other materials
- *    provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without specific prior written
- *    permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include "syscap_tool.h"
-#include "endian_internal.h"
-#include "cJSON.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
 #include <libgen.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <securec.h>
+#include "endian_internal.h"
+#include "cJSON.h"
+#include "syscap_tool.h"
 
 typedef struct ProductCompatibilityIDHead {
     uint16_t apiVersion : 15;
@@ -62,7 +47,7 @@ typedef struct RequiredProductCompatibilityIDHead {
     do { \
         printf("ERROR: in file %s at line %d -> ", __FILE__, __LINE__); \
         printf(__VA_ARGS__); \
-    } while (0);
+    } while (0)
 
 static void FreeContextBuffer(char *contextBuffer)
 {
@@ -100,11 +85,11 @@ static uint32_t GetFileContext(char *inputFile, char **contextBufPtr, uint32_t *
     if (ret != 1) {
         PRINT_ERR("read file(%s) failed, errno = %d\n", inputFile, errno);
         FreeContextBuffer(contextBuffer);
-        fclose(fp);
+        (void)fclose(fp);
         return -1;
     }
     contextBuffer[statBuf.st_size] = '\0';
-    fclose(fp);
+    (void)fclose(fp);
 
     *contextBufPtr = contextBuffer;
     *bufferLen = statBuf.st_size + 1;
@@ -144,11 +129,11 @@ static int32_t ConvertedContextSaveAsFile(char *outDirPath, char *filename, char
     ret = fwrite(convertedBuffer, bufferLen, 1, fp);
     if (ret != 1) {
         PRINT_ERR("can`t write file(%s),errno = %d\n", fileFullPath, errno);
-        fclose(fp);
+        (void)fclose(fp);
         return -1;
     }
 
-    fclose(fp);
+    (void)fclose(fp);
 
     return 0;
 }
