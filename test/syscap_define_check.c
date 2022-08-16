852b210f79e210cbedd9e7dd715008ed2ffbd85f
yudechen <chenyude@huawei.com>
2022-08-16 20:42:36 +0800

    add syscap define check.
    Signed-off-by: yudechen <chenyude@huawei.com>
    Change-Id: I26d83d022570cedfd53fcfb9b9e83ac68fe3ac86

--- test/syscap_define_check.c
+++ test/syscap_define_check.c
@@ -6,16 +6,17 @@
 int main()
 {
     size_t size = sizeof(g_arraySyscap) / sizeof(SyscapWithNum);
-    // printf("size = %lu\n", size);
-    int flag = 0;
+    size_t flag = 0;
 
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         if (g_arraySyscap[i].num != i) {
-            printf("[Error]: %s -> num(%u) should be %d.\n", g_arraySyscap[i].str, g_arraySyscap[i].num, i);
+            printf("[Error]: %s -> num(%u) should be %lu.\n", g_arraySyscap[i].str, g_arraySyscap[i].num, i);
             flag++;
         }
     }
-    // if (flag == 0) return 0;
-    // else exit(1);
-    exit(1);
+    if (flag == 0) {
+        return 0;
+    } else {
+        return -1;
+    }
 }
