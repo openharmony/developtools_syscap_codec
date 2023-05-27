16319a4819698eede4b046cf85783700ebddca61
yudechen <chenyude@huawei.com>
2023-05-27 14:26:01 +0800

    add copyright statement.
    Signed-off-by: yudechen <chenyude@huawei.com>
    Change-Id: I55018e69ebbb683e0ad1d6c9e34fcd18cc4f54d4

--- tools/syscap_config_merge.py
+++ tools/syscap_config_merge.py
@@ -1,4 +1,18 @@
 #! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 '''
 注意：
 1. base 的枚举定义，须在末尾标记最大值。
@@ -8,7 +22,7 @@
 import argparse
 
 licence = '''/*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
