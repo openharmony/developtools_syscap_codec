b3fbada11ef64a7353ca405b326e205a075f4148
tongkai0808fh <tongkai5@huawei.com>
2024-05-17 08:20:19 +0000

    修改interfaces/inner_api/syscap_interface.c文件，内存变量outputArray在内存释放后赋值空值 247行内存变量outputArray在内存释放后赋值空值
    Signed-off-by: tongkai0808fh <tongkai5@huawei.com>

--- interfaces/inner_api/syscap_interface.c
+++ interfaces/inner_api/syscap_interface.c
@@ -246,6 +246,7 @@ bool DecodePrivateSyscap(char *input, char (**output)[SINGLE_SYSCAP_LEN], int *o
             *bufferPos = '\0';
             if (sprintf_s(*outputArray, SINGLE_SYSCAP_LEN, "SystemCapability.%s", buffer) == -1) {
                 free(outputArray);
+                outputArray = NULL;
                 return false;
             }
             bufferPos = buffer;
