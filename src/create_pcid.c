dd045e357e2b162bd70ee6eb711201a3da932200
Luming <luming29@huawei.com>
2024-08-20 01:51:54 +0000

    update src/create_pcid.c.
    Signed-off-by: Luming <luming29@huawei.com>

--- src/create_pcid.c
+++ src/create_pcid.c
@@ -759,9 +759,7 @@ int32_t DecodeStringPCIDToJson(char *input, char *outDirPath)
     ret = 0;
 
 FAILED:
-    if (jsonBuffer != NULL) {
-        cJSON_free(jsonBuffer);
-    }
+    cJSON_free(jsonBuffer);
     SafeFree(priSyscapStr);
     cJSON_Delete(sysCapObj);
     cJSON_Delete(rootObj);
