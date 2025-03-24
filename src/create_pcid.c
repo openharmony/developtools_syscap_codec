50970be76d515c7e9db811652adc6a3c62228a95
zhaohang <zhaohang44@huawei.com>
2025-03-24 16:16:21 +0800

    修改安全告警
    Signed-off-by: zhaohang <zhaohang44@huawei.com>

--- src/create_pcid.c
+++ src/create_pcid.c
@@ -379,6 +379,11 @@ static int32_t GetPriSyscap(PCIDMain *pcidMain, cJSON *sysCapObject, int32_t con
     char *tempPriSyscapStr = priSyscapStr;
     char *ptrPrivateSyscap = (char *)(pcidMain + 1);
     while (*ptrPrivateSyscap != '\0') {
+        size_t currentLen = tempPriSyscapStr - priSyscapStr;
+        if (currentLen >= SINGLE_SYSCAP_LEN - 1) {
+            PRINT_ERR("Syscap name exceeds maximum length\n");
+            return GetPriSyscapResult(capVectorPtr, -1);
+        }
         if (*ptrPrivateSyscap == ',') {
             *tempPriSyscapStr = '\0';
             int32_t ret = sprintf_s(fullCapStr, SINGLE_SYSCAP_LEN, "SystemCapability.%s", priSyscapStr);
