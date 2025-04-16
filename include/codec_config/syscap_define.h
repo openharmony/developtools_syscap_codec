8812baf746f09082aeafacb411073551be167684
lixing <lixing141@huawei.com>
2025-04-16 02:15:43 +0000

    hidumper add abandoned
    Signed-off-by: lixing <lixing141@huawei.com>

--- include/codec_config/syscap_define.h
+++ include/codec_config/syscap_define.h
@@ -452,7 +452,7 @@ const static SyscapWithNum g_arraySyscap[] = {
     {"SystemCapability.Graphic.Wms", GRAPHIC_WMS},
     {"SystemCapability.HiviewDFX.HiAppEvent", HIVIEWDFX_HIAPPEVENT},
     {"SystemCapability.HiviewDFX.HiChecker", HIVIEWDFX_HICHECKER},
-    {"SystemCapability.HiviewDFX.HiDumper", HIVIEWDFX_HIDUMPER},
+    {"SystemCapability.HiviewDFX.HiDumper", HIVIEWDFX_HIDUMPER}, // abandoned
     {"SystemCapability.HiviewDFX.HiEventLite", HIEVENTKLITE},
     {"SystemCapability.HiviewDFX.HiLog", HIVIEWDFX_HILOG},
     {"SystemCapability.HiviewDFX.HiLogLite", HIVIEWDFX_HILOGLITE},
