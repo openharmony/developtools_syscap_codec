ad8030bd5fcf285730480bf63172eed7f22a8d65
LeechyLiang <liangweihong1@huawei.com>
2025-06-09 10:22:50 +0800

    FixSyscapErrorIn0328
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/ICIYQU
    Signed-off-by: LeechyLiang <liangweihong1@huawei.com>
    Change-Id: If22c5772abb0877a6256a3b05fa9f46475bc5a94

--- include/codec_config/syscap_define.h
+++ include/codec_config/syscap_define.h
@@ -380,6 +380,7 @@ typedef enum SystemCapabilityNum {
     MULTIMEDIA_AVSESSION_AVINPUTCAST,
     GAME_GAMECONTROLLER,
     DISTRIBUTEDDATAMANAGER_DATAINTELLIGENCE_CORE,
+    ARKCOMPILER_ANI,
     // Add before here
     SYSCAP_BASIC_END = 500,
 } SyscapNum;
@@ -744,6 +745,7 @@ const static SyscapWithNum g_arraySyscap[] = {
     {"SystemCapability.DistributedSched.AppCollaboration", DISTRIBUTEDSCHED_APPCOLLABORATION},
     {"SystemCapability.Game.GameController", GAME_GAMECONTROLLER},
     {"SystemCapability.DistributedDataManager.DataIntelligence.Core", DISTRIBUTEDDATAMANAGER_DATAINTELLIGENCE_CORE},
+    {"SystemCapability.ArkCompiler.ANI", ARKCOMPILER_ANI},
 };
 
 #ifdef __cplusplus
