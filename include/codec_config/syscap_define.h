0fafa9cdb0967a991ab327332855d795f4153d0f
hongtao <wanghongtao15@huawei.com>
2025-07-11 02:47:16 +0000

    fix USERSTATUS
    Signed-off-by: hongtao <wanghongtao15@huawei.com>

--- include/codec_config/syscap_define.h
+++ include/codec_config/syscap_define.h
@@ -390,7 +390,7 @@ typedef enum SystemCapabilityNum {
     MULTIMEDIA_MEDIA_LOWPOWERAVSINK,
     COMMUNICATION_NETMANAGER_EAP,
     RESOURCESCHEDULE_QOS_CORE,
-    MULTIMODALAWARENESS_UserStatus,
+    MULTIMODALAWARENESS_USERSTATUS,
     // Add before here
     SYSCAP_BASIC_END = 500,
 } SyscapNum;
@@ -765,7 +765,7 @@ const static SyscapWithNum g_arraySyscap[] = {
     {"SystemCapability.Multimedia.Media.LowPowerAVSink", MULTIMEDIA_MEDIA_LOWPOWERAVSINK},
     {"SystemCapability.Communication.NetManager.Eap", COMMUNICATION_NETMANAGER_EAP},
     {"SystemCapability.Resourceschedule.QoS.Core", RESOURCESCHEDULE_QOS_CORE},
-    {"SystemCapability.MultimodalAwareness.UserStatus", MULTIMODALAWARENESS_UserStatus},
+    {"SystemCapability.MultimodalAwareness.UserStatus", MULTIMODALAWARENESS_USERSTATUS},
 };
 
 #ifdef __cplusplus
