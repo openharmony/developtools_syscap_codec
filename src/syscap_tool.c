e48db381fd9a479b00f25e6ce4573b9cab12271e
xuezhou_yan <yangang20@huawei.com>
2025-08-06 16:27:03 +0800

    [Bug]: 告警处理，清理没有释放的内存 https://gitee.com/openharmony/developtools_syscap_codec/issues/ICRCLU
    Signed-off-by: xuezhou_yan <yangang20@huawei.com>

--- src/syscap_tool.c
+++ src/syscap_tool.c
@@ -698,7 +698,7 @@ static int32_t CompareVersion(uint32_t *pcidOsArray, uint32_t *rpcidOsAarry)
 }
 
 static void FreePointerMemoryEnd(char *pcidContent, char *rpcidContent,
-                                char *pcidPriSyscap, char *rpcidPriSyscap, uint32_t type)
+                                 char *pcidPriSyscap, char *rpcidPriSyscap, uint32_t type)
 {
     printf("After the compare cmd is executed, relase the memory.\n");
     if (type == TYPE_FILE) {
